9ea85c8758b7313d93081e138003f46ff737b3f3
108360224 <56162067+108360224@users.noreply.github.com>
2021-07-30 12:03:45 +0800

    Update gradiandecent.cpp

--- gradiandecent.cpp
+++ gradiandecent.cpp
@@ -2,51 +2,42 @@
 #include <cmath>
 
 using namespace std;
-
-GradianDecent::GradianDecent(functionlist funclist,Xparam *x,double esp,double lr){
+using namespace SAD;
+GradianDecent::GradianDecent(functionlist funclist,vector<complexnumber> &x,double lr){
     this -> funclist = funclist;
-    this -> esp = esp;
+
     this -> lr = lr;
-    this -> x = x;
+    this -> x = &x;
 }
-Xparam GradianDecent::diffunc(functiontype func){
-    Xparam dy = *(this->x);
 
-    Xparam xplusdx = *(this->x);
-    for(unsigned i=0;i<(*(this->x)).size();i++){
-        xplusdx[i] = (*(this->x))[i]+this->esp;
+vector<complexnumber> GradianDecent::particularDiff(vector<complexnumber> x,functiontype func){
+    vector<complexnumber> y;
+    for(unsigned i=0;i<x.size();i++){
+        vector <ADV> ax;
+        for(unsigned j=0;j<x.size();j++){
+            if(i==j){
+                ax.emplace_back(x[i],1);
+            }else{
+                ax.emplace_back(x[i],0);
+            }
 
-        double dre=(real(pow(func(xplusdx),2))-real(pow(func(*(this->x)),2)))/this->esp;
-
-        double dim=(imag(pow(func(xplusdx),2))-imag(pow(func(*(this->x)),2)))/this->esp;
-        complexnumber comp(dre,dim);
-        dy[i] = comp;
-    }
-    return dy;
-}
-void GradianDecent::gradiandecent(){
-    for(unsigned i=0;i<this->funclist.size();i++){
-        Xparam dy = diffunc((this->funclist)[i]);
-        for(unsigned i=0;i<(*(this->x)).size();i++){
-            (*(this->x))[i] = (*(this->x))[i] - lr*conj(dy[i]);
         }
+        y.emplace_back(pow(func(ax),2).dval);
     }
-
-
+    return y ;
 
 }
-
-void GradianDecent::run(int times){
-
+void GradianDecent::gradiandecent()
+{
+    vector<complexnumber> dy=particularDiff(*x,funclist[0]);
+    for(unsigned i=0;i<(*x).size();i++){
+        (*x)[i]=(*x)[i]-lr*dy[i];
+    }
+}
+void GradianDecent::run(int times)
+{
     for(int i=0;i<times;i++){
-        this->gradiandecent();
-        for(unsigned i=0;i<(*(this->x)).size();i++){
-
-
-        }
-
+        gradiandecent();
     }
-
 }
-
 
